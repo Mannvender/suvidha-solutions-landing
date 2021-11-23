789b61e3693a2caf14eab1f3da161f46b8a2c1e7
Shrey Pandey <shreypandey27@gmail.com>
2021-11-23 15:42:31 +0530

    merge with master

--- messages.js
+++ messages.js
@@ -1,5 +1,5 @@
 export const DEFAULT_ERROR_MESSAGE =
-  "Something went wrong! We are looking into it.";
+  'Something went wrong! We are looking into it.';
 
 export const ROADMAP = [
   {
@@ -33,4 +33,32 @@ export const ROADMAP = [
     what: `69 of our Cheetahs will be selected to be a part of the movie Chariot Cheetahs
      - a battle royale movie for being the last champion cheetah charioting among asteroids in hell`,
   },
+];
+
+export const FAQS = [
+  {
+    question: 'How to mint a Cheating Cheetah?',
+    answer: "We will be launching on Sol blockchain, you'll need a solana wallet like phantom or sollet.",
+    height: ['56px', '40px'],
+  },
+  {
+    question: 'What is the price of a Cheating Cheetah?',
+    answer: "0.49 Sol during the presale. 0.69 Sol during the public sale.",
+    height: ['56px', '40px'],
+  },
+  {
+    question: 'What is the total supply?',
+    answer: "2969 CC only. 269 for presale and 2700 for public sale.",
+    height: ['56px', '40px'],
+  },
+  {
+    question: 'When is the launch?',
+    answer: "Presale: 5th December. Public sale: 6th December.",
+    height: ['56px', '40px'],
+  },
+  {
+    question: 'What is the market place?',
+    answer: 'CCs will be listed on 6th December on MagicEden, DigitalEyes & Alpha.Art',
+    height: ['56px', '40px'],
+  }
 ];
