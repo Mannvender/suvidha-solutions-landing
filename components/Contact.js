ae3665bbdab29719f0117afdd8ed0377453ca1e0
mannvender <mannvender001@gmail.com>
2022-04-15 14:34:32 +0530

    update contact

--- components/Contact.js
+++ components/Contact.js
@@ -3,7 +3,22 @@ import { Heading, Box, Flex, Text } from "rebass";
 import styled, { useTheme } from "styled-components";
 import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai";
 import { FaPhoneSquareAlt } from "react-icons/fa";
-import { MdEmail, MdMessage } from "react-icons/md";
+import { MdEmail, MdMessage, MdLocationOn } from "react-icons/md";
+import { CONTACT } from "messages";
+
+const getIcon = (name) => {
+  const { colors } = useTheme();
+  switch (name) {
+    case "Phone":
+      return <FaPhoneSquareAlt fill={colors.light} size={20} />;
+    case "Email":
+      return <MdEmail fill={colors.light} size={20} />;
+    case "Address":
+      return <MdLocationOn fill={colors.light} size={20} />;
+    case "Send a message":
+      return <MdMessage fill={colors.light} size={20} />;
+  }
+};
 
 export const StyledFlex = styled(Flex)`
   color: ${(props) => props.theme.colors.light3};
@@ -50,121 +65,61 @@ const FaqsSection = () => {
         width={["100%"]}
         mb={[5]}
       >
-        <Box
-          p={[4]}
-          flexBasis="100%"
-          sx={{
-            backgroundColor: colors.dark3,
-            cursor: "pointer",
-          }}
-          mb={[2]}
-          onClick={() => handleClick("phone")}
-        >
-          <StyledFlex justifyContent="space-between">
-            <Box display={"flex"} flexDirection="row">
-              <FaPhoneSquareAlt fill={colors.light} size={20} />
-              <Text mr={[3]} fontSize={[2, 3]} ml={[2]}>
-                Phone
-              </Text>
-            </Box>
-            {activeSections.includes("phone") ? (
-              <AiOutlineMinus size={20} />
-            ) : (
-              <AiOutlinePlus size={20} />
-            )}
-          </StyledFlex>
+        {CONTACT.map((item) => (
           <Box
-            height={activeSections.includes("phone") ? "40px" : "0px"}
+            p={[4]}
+            flexBasis="100%"
             sx={{
-              overflow: "hidden",
-              transition: "all 0.4s ease",
+              backgroundColor: colors.dark3,
+              cursor: "pointer",
             }}
+            mb={[2]}
+            onClick={() => handleClick(item.name)}
           >
-            <Text mt={[3]} color={colors.accent5} fontSize={[1, 4]}>
-              7127855864
-            </Text>
-          </Box>
-        </Box>
-        <Box
-          p={[4]}
-          flexBasis="100%"
-          sx={{
-            backgroundColor: colors.dark3,
-            cursor: "pointer",
-          }}
-          mb={[2]}
-          onClick={() => handleClick("email")}
-        >
-          <StyledFlex justifyContent="space-between">
-            <Box display={"flex"} flexDirection="row">
-              <MdEmail fill={colors.light} size={22} />
-              <Text mr={[3]} fontSize={[2, 3]} ml={[2]}>
-                Email
-              </Text>
-            </Box>
-            {activeSections.includes("email") ? (
-              <AiOutlineMinus size={20} />
-            ) : (
-              <AiOutlinePlus size={20} />
-            )}
-          </StyledFlex>
-          <Box
-            height={activeSections.includes("email") ? "48px" : "0px"}
-            sx={{
-              overflow: "hidden",
-              transition: "all 0.4s ease",
-            }}
-          >
-            <Text mt={[3]} color={colors.accent5} fontSize={[1, 4]}>
-              dalal.naresh@gmail.com
-            </Text>
-          </Box>
-        </Box>
-        <Box
-          p={[4]}
-          flexBasis="100%"
-          sx={{
-            backgroundColor: colors.dark3,
-            cursor: "pointer",
-          }}
-          mb={[2]}
-          onClick={() => handleClick("message")}
-        >
-          <StyledFlex justifyContent="space-between">
-            <Box display={"flex"} flexDirection="row">
-              <MdMessage fill={colors.light} size={22} />
-              <Text mr={[3]} fontSize={[2, 3]} ml={[2]}>
-                Send a message
-              </Text>
-            </Box>
-            {activeSections.includes("message") ? (
-              <AiOutlineMinus size={20} />
-            ) : (
-              <AiOutlinePlus size={20} />
-            )}
-          </StyledFlex>
-          <Box
-            height={activeSections.includes("message") ? "947px" : "0px"}
-            sx={{
-              overflow: "hidden",
-              transition: "all 0.4s ease",
-            }}
-            display="flex"
-            justifyContent={"center"}
-            pt={[3]}
-          >
-            <iframe
-              src="https://docs.google.com/forms/d/e/1FAIpQLScdW6ExhTts62ZmoPSaSINuBuuUft6ZjwLMl-fqHAZGzgVGjQ/viewform?embedded=true"
-              width="640"
-              height="947"
-              frameborder="0"
-              marginheight="0"
-              marginwidth="0"
+            <StyledFlex justifyContent="space-between">
+              <Box display={"flex"} flexDirection="row">
+                {getIcon(item.name)}
+                <Text mr={[3]} fontSize={[2, 3]} ml={[2]}>
+                  {item.name}
+                </Text>
+              </Box>
+              {activeSections.includes(item.name) ? (
+                <AiOutlineMinus size={20} />
+              ) : (
+                <AiOutlinePlus size={20} />
+              )}
+            </StyledFlex>
+            <Box
+              height={activeSections.includes(item.name) ? item.height : "0px"}
+              sx={{
+                overflow: "hidden",
+                transition: "all 0.4s ease",
+              }}
+              display="flex"
+              flexDirection={"column"}
+              justifyContent={item.align}
             >
-              Loading…
-            </iframe>
+              {!item.iframe &&
+                item.value.map((v) => (
+                  <Text mt={[3]} color={colors.accent5} fontSize={[1, 4]}>
+                    {v}
+                  </Text>
+                ))}
+              {item.iframe && (
+                <iframe
+                  src={item.link}
+                  width="640"
+                  height="947"
+                  frameborder="0"
+                  marginheight="0"
+                  marginwidth="0"
+                >
+                  Loading…
+                </iframe>
+              )}
+            </Box>
           </Box>
-        </Box>
+        ))}
       </Flex>
     </Flex>
   );
--- messages.js
+++ messages.js
@@ -25,3 +25,30 @@ export const SERVICES = [
     color: "primary",
   },
 ];
+
+export const CONTACT = [
+  {
+    name: "Address",
+    value: [
+      "K2 Tower, Basement Floor, Bhiwani, Haryana",
+      "563, 2nd Floor, Gurugram, Haryana",
+    ],
+    height: "90px",
+    align: "left",
+  },
+  { name: "Phone", value: ["+91 9910017374"], height: "40px", align: "left" },
+  {
+    name: "Email",
+    value: ["sales@suvidhasolutions.com"],
+    height: "48px",
+    align: "left",
+  },
+  {
+    name: "Send a message",
+    value: "",
+    iframe: true,
+    link: "https://docs.google.com/forms/d/e/1FAIpQLScdW6ExhTts62ZmoPSaSINuBuuUft6ZjwLMl-fqHAZGzgVGjQ/viewform?embedded=true",
+    height: "947px",
+    align: "center",
+  },
+];
